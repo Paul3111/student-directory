b6d7a2f50911423299998041541d5134e508f1ff
Paul Lazar <lazar_paul_2006@yahoo.com>
2023-02-07 18:35:48 +0000

    added count var

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+student_count = 11
 puts "The students of Villains Academy"
 puts "------------------"
 puts "Dr. Hannibal Lecter"
@@ -11,3 +12,4 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+puts "Overall, we have #{student_count} great students"

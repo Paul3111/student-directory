3725d19a00b39221799a207c66ed0f874f92a52e
Paul Lazar <lazar_paul_2006@yahoo.com>
2023-02-08 20:22:41 +0000

    cohort is no longer hardcoded and default value is set

--- directory.rb
+++ directory.rb
@@ -1,45 +1,25 @@
-#def input_students
-#    puts "Please enter the names of the students"
-#    puts "To finish, just hit return twice"
-#    students = []
-#    name = gets.chomp
+def input_students
+    puts "Please enter the names of the students"
+    name = gets.chomp
+    puts "Please enter the cohort"
+    answer = gets.chomp
+    answer == "" ? cohort = "february" : cohort = answer
+    puts "To finish, just hit return twice"
+    students = []
     
-#    while !name.empty? do
-#        students << {name: name, cohort: :november}
-#        puts "Now we have #{students.count} students"
+    while !name.empty? || !answer.empty? do
+        students << {name: name, cohort: cohort}
+        puts "Now we have #{students.count} students\n"
         
-#        name = gets.chomp
-#    end
-#    students
-#end
-
-#def print_header
-#    puts "The students of Villains Academy"
-#    puts "-------------"
-#end
-
-#def print_footer(names)
-#    puts "Overall, we have #{names.count} great students"
-#end
-
-#students = input_students
-#print_header
-#print(students)
-#print_footer(students)
-
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november, cob: "UK"},
-  {name: "Darth Vader", cohort: :november, cob: "Galactia"},
-  {name: "Nurse Ratched", cohort: :november, cob: "Switzerland"},
-  {name: "Michael Corleone", cohort: :november, cob: "Italy"},
-  {name: "Alex DeLarge", cohort: :november, cob: "UK"},
-  {name: "The Wicked Witch of the West", cohort: :november, cob: "N/A"},
-  {name: "Terminator", cohort: :november, cob: "Future Land"},
-  {name: "Freddy Krueger", cohort: :november, cob: "USA"},
-  {name: "The Joker", cohort: :november, cob: "USA"},
-  {name: "Joffrey Baratheon", cohort: :november, cob: "UK"},
-  {name: "Norman Bates", cohort: :november, cob: "Norway"}
-]
+        puts "Please enter the names of the students"
+        name = gets.chomp
+        puts "Please enter the cohort"
+        answer = gets.chomp
+        answer == "" ? cohort = "february" : cohort = answer
+        puts "To finish, just hit return twice"
+    end
+    students
+end
 
 def print_header
     puts "The students of Villains Academy"
@@ -47,18 +27,10 @@ def print_header
 end
 
 def print_footer(names)
-    puts "\nOverall, we have #{names.count} great students"
+    puts "Overall, we have #{names.count} great students"
 end
 
-def print(students)
-    counter = 0
-    while counter < students.length
-        puts "#{students[counter][:name]} 
-        (#{students[counter][:cohort].to_s.center(14)} cohort) - #{students[counter][:cob].center(15)}"
-        counter += 1
-    end
-end
-
+students = input_students
 print_header
-print(students)
+puts(students)
 print_footer(students)
